e24f633b36d622c8e4c5267df94a124f351103e3
Bouh <bouh.abdillahi@polymtl.ca>
2023-02-15 05:00:33 -0500

    q command gives current song, np command doesn't need vote anymore

--- cogs/music.py
+++ cogs/music.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e29758c4f3fb774555dde4316d0cbef7e35330a5674a13ba629f8e6b6b2342c
-size 15419
+oid sha256:1e115580f819808181a2911d04f3dea42c5b684a720ba2f7a163af6c89be8c5e
+size 15556
