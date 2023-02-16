4408e90395484bbddf04ddb877c00c8210409ea0
Bouh <bouh.abdillahi@polymtl.ca>
2023-02-16 04:25:19 -0500

    added loop feature to the bot

--- cogs/music.py
+++ cogs/music.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e115580f819808181a2911d04f3dea42c5b684a720ba2f7a163af6c89be8c5e
-size 15556
+oid sha256:62b0c2bece25264e8ac71fb90b64b00ac87d17e6e830cc712fd033b929474539
+size 17539
