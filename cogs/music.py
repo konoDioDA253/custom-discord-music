b9b5b93f745231e6251532017a7a10b78dfd4236
Bouh <bouh.abdillahi@polymtl.ca>
2023-02-16 04:50:40 -0500

    added some UI shit

--- cogs/music.py
+++ cogs/music.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62b0c2bece25264e8ac71fb90b64b00ac87d17e6e830cc712fd033b929474539
-size 17539
+oid sha256:a7754d88880452056ecbe82a4cd8ea6e2fe1879e94e7c8b6b031ecc6a6919443
+size 18037
